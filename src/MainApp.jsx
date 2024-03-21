bac578189c4ec89b990f164cd980480f9d47dc8a
--replace-all <ignacio.jamett.r@gmail.com>
2024-03-21 17:14:32 -0300

    pages folder created

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,30 +1,9 @@
-import { BirdsList } from "./components/BirdsList";
+import { BirdsPage } from "./pages/BirdsPage";
 
-export function MainApp() {
-  const dummies = [
-    {
-      id: 1,
-      name: "Chincol",
-      description:
-        "Son aves solitarias, que sólo forman parejas en época de reproducción. Se alimentan principalmente de semillas y gusanos.",
-      area: "Praderas y bosques",
-    },
-    {
-      id: 2,
-      name: "Tenca",
-      description:
-        "Gran cantora, puede imitar el sonido de otros pájaros. Suele anidar en árboles con espinas, y pone 3 huevos.",
-      area: "Laderas de cerros con arbustos",
-    },
-  ];
+export const MainApp = () => {
   return (
     <>
-      <div className="container my-4">
-        <h3>Pulmapedia</h3>
-        <div className="row">
-          <BirdsList birds={dummies} />
-        </div>
-      </div>
+      <BirdsPage />
     </>
   );
-}
+};
---
+++ src/pages/BirdsPage.jsx
@@ -0,0 +1,30 @@
+import { BirdsList } from "../components/BirdsList";
+
+export function BirdsPage() {
+  const dummies = [
+    {
+      id: 1,
+      name: "Chincol",
+      description:
+        "Son aves solitarias, que sólo forman parejas en época de reproducción. Se alimentan principalmente de semillas y gusanos.",
+      area: "Praderas y bosques",
+    },
+    {
+      id: 2,
+      name: "Tenca",
+      description:
+        "Gran cantora, puede imitar el sonido de otros pájaros. Suele anidar en árboles con espinas, y pone 3 huevos.",
+      area: "Laderas de cerros con arbustos",
+    },
+  ];
+  return (
+    <>
+      <div className="container my-4">
+        <h3>Pulmapedia</h3>
+        <div className="row">
+          <BirdsList birds={dummies} />
+        </div>
+      </div>
+    </>
+  );
+}
